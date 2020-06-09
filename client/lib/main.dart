cf0693ef66b5e4b7088cbd031286748fb760c51b
StillRosi <nirossi@tsn.at>
2020-06-09 16:09:14 +0200

    oop

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:client/widgets/menu/menu.dart';
+import 'package:client/widgets/tab_controller/tabController.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -20,66 +20,7 @@ class MyApp extends StatelessWidget {
         ),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: TabController()
-    );
-  }
-}
-
-class TabController extends StatefulWidget {
-  @override
-  _TabController createState() => _TabController();
-}
-
-class _TabController extends State<TabController> {
-  @override
-  Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('SOKKA', style: TextStyle(color: Colors.white)),
-          centerTitle: true,
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.account_box, color: Colors.white),
-              onPressed: () => null,
-            ),
-            IconButton(
-              icon: Icon(Icons.settings, color: Colors.white,),
-              onPressed: () => null,
-            ),
-          ],
-        ),
-        bottomNavigationBar: Container(
-          color: Colors.tealAccent[700],
-          child: TabBar(
-            labelColor: Colors.white,
-            unselectedLabelColor: Colors.white70,
-            indicatorSize: TabBarIndicatorSize.tab,
-            indicatorPadding: EdgeInsets.all(5.0),
-            indicatorColor: Colors.tealAccent[100],
-            tabs: [
-              Tab(text: 'Wallet', icon: Icon(Icons.account_balance_wallet)),
-              Tab(text: 'Menus', icon: Icon(Icons.restaurant_menu)),
-              Tab(text: 'Basket', icon: Icon(Icons.shopping_basket)),
-            ],
-          ),
-        ),
-        body: TabBarView(
-          children: <Widget>[
-            Container(child: Icon(Icons.account_balance_wallet, color: Colors.white,)),
-            Container(
-              child: ListView.builder(
-                itemCount: 5,
-                itemBuilder: (BuildContext context, int index) {
-                  return Menu(index + 1);
-                },
-              ),
-            ),
-            Container(child: Icon(Icons.shopping_basket, color: Colors.white,)),
-          ],
-        ),
-      ),
+      home: HomeTabController()
     );
   }
 }
