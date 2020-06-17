4ceda5a7b8e88d9e17a2050b7466c8cad33893df
StillRosi <n.rossi2002@gmail.com>
2020-06-17 08:34:42 +0200

    tabcontroller into safe area

--- client/lib/widgets/tab_controller/tabController.dart
+++ client/lib/widgets/tab_controller/tabController.dart
@@ -42,20 +42,22 @@ class _HomeTabController extends State<HomeTabController> {
             ],
           ),
         ),
-        body: TabBarView(
-          children: <Widget>[
-            Container(child: Icon(Icons.account_balance_wallet, color: Colors.white,)),
-            Container(
-              child: ListView.builder(
-                itemCount: 5,
-                itemBuilder: (BuildContext context, int index) {
-                  return Menu(index + 1);
-                },
+        body: SafeArea(
+          child: TabBarView(
+            children: <Widget>[
+              Container(child: Icon(Icons.account_balance_wallet, color: Colors.white,)),
+              Container(
+                child: ListView.builder(
+                  itemCount: 5,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Menu(index + 1);
+                  },
+                ),
               ),
-            ),
-            Container(child: Icon(Icons.shopping_basket, color: Colors.white,)),
-          ],
-        ),
+              Container(child: Icon(Icons.shopping_basket, color: Colors.white,)),
+            ],
+          ),
+        )
       ),
     );
   }
