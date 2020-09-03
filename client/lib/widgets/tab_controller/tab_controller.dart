9ffdb9ed62cf58fb778069e5577e204fb796f8a3
StillRosi <n.rossi2002@gmail.com>
2020-09-03 22:47:31 +0200

    improved menu and menupanel + adapted tab-controller

--- client/lib/widgets/tab_controller/tab_controller.dart
+++ client/lib/widgets/tab_controller/tab_controller.dart
@@ -1,77 +1,66 @@
 import 'package:flutter/material.dart';
-import 'package:client/widgets/menu/menu.dart';
+import 'package:client/widgets/menu/menu_panel.dart';
 import 'package:client/widgets/account/account.dart';
 
 class HomeTabController extends StatefulWidget {
-  @override
-  _HomeTabController createState() => _HomeTabController();
+    @override
+    _HomeTabController createState() => _HomeTabController();
 }
 
 class _HomeTabController extends State<HomeTabController> {
-  @override
-  Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('SOKKA', style: TextStyle(color: Colors.white)),
-          centerTitle: true,
-          leading: IconButton(
-            icon: Icon(Icons.account_box, color: Colors.white),
-            onPressed: () => Account(),
-          ),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(
-                Icons.settings,
-                color: Colors.white,
-              ),
-              onPressed: () => null,
+    @override
+    Widget build(BuildContext context) {
+        return DefaultTabController(
+            length: 3,
+            child: Scaffold(
+                appBar: AppBar(
+                    title: Text('SOKKA', style: TextStyle(color: Colors.white)),
+                    centerTitle: true,
+                    leading: IconButton(
+                        icon: Icon(Icons.account_box, color: Colors.white),
+                        onPressed: () => Account(),
+                    ),
+                    actions: <Widget>[
+                        IconButton(
+                            icon: Icon(Icons.settings, color: Colors.white),
+                            onPressed: () => null,
+                        ),
+                    ],
+                ),
+                bottomNavigationBar: Container(
+                    color: Colors.tealAccent[700],
+                    child: TabBar(
+                        labelColor: Colors.white,
+                        unselectedLabelColor: Colors.white70,
+                        indicatorSize: TabBarIndicatorSize.tab,
+                        indicatorPadding: EdgeInsets.all(5.0),
+                        indicatorColor: Colors.tealAccent[100],
+                        tabs: [
+                            Tab(text: 'Wallet', icon: Icon(Icons.account_balance_wallet)),
+                            Tab(text: 'Menus', icon: Icon(Icons.restaurant_menu)),
+                            Tab(text: 'Basket', icon: Icon(Icons.shopping_basket)),
+                        ],
+                    ),
+                ),
+                body: SafeArea(
+                    child: TabBarView(
+                        children: <Widget>[
+                            Container(
+                                child: Icon(Icons.account_balance_wallet, color: Colors.white),
+                            ),
+                            Container(
+                                child: ListView.builder(
+                                    itemCount: 5,
+                                    itemBuilder: (BuildContext context, int index) => MenuPanel(index + 1)
+                                ),
+                            ),
+                            Container(
+                                child: Icon(Icons.shopping_basket, color: Colors.white),
+                            ),
+                        ],
+                    ),
+                ),
             ),
-          ],
-        ),
-        bottomNavigationBar: Container(
-          color: Colors.tealAccent[700],
-          child: TabBar(
-            labelColor: Colors.white,
-            unselectedLabelColor: Colors.white70,
-            indicatorSize: TabBarIndicatorSize.tab,
-            indicatorPadding: EdgeInsets.all(5.0),
-            indicatorColor: Colors.tealAccent[100],
-            tabs: [
-              Tab(text: 'Wallet', icon: Icon(Icons.account_balance_wallet)),
-              Tab(text: 'Menus', icon: Icon(Icons.restaurant_menu)),
-              Tab(text: 'Basket', icon: Icon(Icons.shopping_basket)),
-            ],
-          ),
-        ),
-        body: SafeArea(
-          child: TabBarView(
-            children: <Widget>[
-              Container(
-                child: Icon(
-                  Icons.account_balance_wallet,
-                  color: Colors.white,
-                ),
-              ),
-              Container(
-                child: ListView.builder(
-                  itemCount: 5,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Menu(index + 1);
-                  },
-                ),
-              ),
-              Container(
-                child: Icon(
-                  Icons.shopping_basket,
-                  color: Colors.white,
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
+        );
+    }
 }
