834c15cb6aed9a558785a727adbfa52c270f2186
StillRosi <n.rossi2002@gmail.com>
2020-09-23 11:52:58 +0200

    package rename

---
+++ client/android/app/src/main/kotlin/tirol/htlanichstrasse/sokka/SplashScreenFragment.kt
@@ -0,0 +1,24 @@
+
+// package com.example.client
+
+/**
+ * @author Nicolaus Rossi
+ * @since 2020-06-16
+ */
+
+ /*
+import androidx.annotation.NonNull;
+
+class TransitionedSplashScreen: SplashScreen() {
+    lateinit private var mySplashView: MySplashView;
+
+    @Nullable
+    override fun createSplashView(@NonNull context: Context, @Nullable savedInstanceState: Bundle): View {
+        return MySplashView(context); 
+    }
+
+    override fun transitionToFlutter(@NonNull onTransitionComplete: Runnable) {
+        onTransitionComplete.run();
+    }
+}
+*/
