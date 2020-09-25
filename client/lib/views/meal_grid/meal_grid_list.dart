4bc58e30daf986c9cac7853e2910e2f4a93f82df
StillRosi <n.rossi2002@gmail.com>
2020-09-25 21:54:26 +0200

    meal grid for standalone meals

---
+++ client/lib/views/meal_grid/meal_grid_list.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MealGridList extends StatefulWidget {
+
+    MealGridList();
+
+    @override
+    _MealGridListState createState() => new _MealGridListState();
+
+}
+
+class _MealGridListState extends State<MealGridList> {
+    @override 
+    Widget build(BuildContext context) {
+        return new Center();
+    }
+}
