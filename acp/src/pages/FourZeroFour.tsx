378e83e3c9b657e6b21624eaaa94d4eaceea3024
Bemoty <jos.winkler@tsn.at>
2020-09-28 12:36:42 +0200

    better 404

--- acp/src/pages/FourZeroFour.tsx
+++ acp/src/pages/FourZeroFour.tsx
@@ -1,13 +1,19 @@
 import React, { FunctionComponent } from 'react';
+import sokkasad from '../images/sokkasad.svg';
 
 interface FourZeroFourProps {
 
 }
 
 const FourZeroFour: FunctionComponent<FourZeroFourProps> = (props) => {
-    return (<div className="app" >
-        <h1>This page does not exist.</h1>
-    </div>);
+    return (<div className="app fourzerofour">
+        <div className="sokkasad">
+            <img src={sokkasad} alt="Sokka Logo sad"/>
+        </div>
+        <h1>Big oof</h1>
+        <p className="text-muted">Seems like you have reached a dead end. Sorry!</p>
+        <a href="/" className="btn btn-secondary">Back to home</a>
+    </div >);
 }
 
 export default FourZeroFour;
