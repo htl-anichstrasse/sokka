b1f71f5ffa9054f131c5f243b5b750e706190307
StillRosi <n.rossi2002@gmail.com>
2020-10-26 20:17:26 +0100

    network -> network_wrapper for decoding request responses

--- client/lib/util/network_wrapper.dart
+++ client/lib/util/network_wrapper.dart
@@ -2,23 +2,24 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-class Network {
-    static Network _instance = new Network.internal();
-    factory Network() => _instance;
+class NetworkWrapper {
+    /// The Dart way of creating a singleton.
+    static NetworkWrapper _instance = new NetworkWrapper.internal();
+    factory NetworkWrapper() => _instance;
 
     final JsonDecoder _jsonDecoder = new JsonDecoder();
 
-    Future<dynamic> get(final String url) {
-        return http.get(url).then((http.Response response) {
+    Future<dynamic> get(final String url) async {
+        return await http.get(url).then((http.Response response) {
             if (response.statusCode < 200 || response.statusCode > 400 || json == null) {
                 throw new Exception("Error fetching data from $url");
             }
             return this._jsonDecoder.convert(response.body);
         });
     }
-    
-    Future<dynamic> post(final String url, {final Map headers, body, encoding}) {
-        return http
+
+    Future<dynamic> post(final String url, {final Map headers, body, encoding}) async {
+        return await http
             .post(
                 url,
                 body: body,
@@ -33,5 +34,5 @@ class Network {
             });
     }
 
-    Network.internal();
+    NetworkWrapper.internal();
 }
