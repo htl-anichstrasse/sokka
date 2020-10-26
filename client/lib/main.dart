05ad638216619e176439f9075f66266455e72a11
StillRosi <n.rossi2002@gmail.com>
2020-10-26 16:30:16 +0100

    tab-controller => home_screen

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:client/views/tab_controller.dart';
+import 'package:client/views/home_screen.dart';
 import 'package:client/styles/theme/app_themes.dart';
 
 void main() async => runApp(Sokka());
