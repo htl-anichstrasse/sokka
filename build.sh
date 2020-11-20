532266cc555ab7776f74d7883262539fbb162f9a
bemoty <jos.winkler@tsn.at>
2020-11-20 17:26:30 +0000

    fix debug build!

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ docker build -t htl-anichstrasse/sokka:v1 server
 # build acp
 sed -i "s/const[[:space:]]debug[[:space:]]=[[:space:]]true;/const debug = false;/g" ./acp/src/Util.ts # disable debug
 cd acp && npm install && npm run build
-sed -i "s/const[[:space:]]debug[[:space:]]=[[:space:]]true;/const debug = false;/g" ./acp/src/Util.ts ./acp/src/Util.ts # enable debug
+sed -i "s/const[[:space:]]debug[[:space:]]=[[:space:]]false;/const debug = true;/g" ./acp/src/Util.ts # enable debug
