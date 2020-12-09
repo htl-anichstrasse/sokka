1789bd5c3fe37a4757f28c5adad3caeea0fec50b
Bemoty <jos.winkler@tsn.at>
2020-12-09 09:39:29 +0100

    use react fragment

--- acp/src/index.tsx
+++ acp/src/index.tsx
@@ -1,5 +1,5 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import 'animate.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
@@ -8,9 +8,9 @@ import './index.css';
 
 // Render react base app
 ReactDOM.render(
-  <React.StrictMode>
+  <React.Fragment>
     <App />
-  </React.StrictMode>,
+  </React.Fragment>,
   document.getElementById('root')
 );
 
