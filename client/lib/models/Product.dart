2be6248717960acc427fb87c5f41709d28f558e2
Nicho <n.rossi2002@gmail.com>
2020-12-30 23:35:03 +0100

    some improvement with regards to OOP

---
+++ client/lib/models/Product.dart
@@ -0,0 +1,11 @@
+abstract class Product {
+    int get getIndex;
+
+    String get getTitle;
+    set setTitle(final String title);
+
+    double get getPrice;
+    set setPrice(final double price);
+
+
+}
