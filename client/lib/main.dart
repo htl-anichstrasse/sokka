5d57acc2fb20cf5303605542c5b43b9bb6a3927e
Nicho <n.rossi2002@gmail.com>
2020-12-30 23:25:09 +0100

    trying to fix non-saving session-token

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:client/services/UserAPIService.dart';
+import 'package:client/services/UserAuth.dart';
+import 'package:client/util/CookieStorage.dart';
 import 'package:client/util/routes.dart';
-import 'package:client/util/SessionTokenStorage.dart';
 import 'package:flutter/material.dart';
 import 'package:client/styles/theme/AppThemes.dart';
 import 'package:flutter/services.dart';
@@ -8,35 +8,42 @@ import 'package:flutter/services.dart';
 void main() async => runApp(Sokka());
 
 class Sokka extends StatelessWidget {
-    final SessionTokenStorage _sessionTokenStorage = new SessionTokenStorage();
-    final UserAPIService _userAPIService = new UserAPIService(); 
-
-    String _email;
-    String _sessionToken;
-    bool _sessionTokenIsValid = false;
-
-    String _initialRoute;
+    final CookieStorage _cookieStorage = new CookieStorage();
+    final UserAuth _userAuth = new UserAuth(); 
 
     @override
     Widget build(BuildContext context) {
-        /// Check if a email is given.
-        this._sessionTokenStorage.getSessionToken('email').then((email) => this._email = email);
+        this._cookieStorage.getSessionToken().then((token)
+            => print('token on startup: $token'));
 
-        /// Check if a session token is given.
-        this._sessionTokenStorage.getSessionToken('sessionToken').then((token) => this._sessionToken = token);
+        String email;
+        String sessionToken;
+        bool tokenIsValid = false;
 
-        /// Check if token is valid.
-        this._userAPIService.validateSessionToken(this._sessionToken, this._email)
-            .then((isValid) => this._sessionTokenIsValid = isValid);
+        String initialRoute;
 
-        this._sessionTokenIsValid ? this._initialRoute = '/' : this._initialRoute = '/login';
-        
+        this._cookieStorage.getEmail().then((emailAddress)
+            => email = emailAddress);
+        print('email: $email');
+        this._cookieStorage.getSessionToken().then((token)
+            => sessionToken = token);
+
+        this._userAuth.validateSessionToken(sessionToken, email)
+            .then((isValid) => tokenIsValid = isValid);
+
+        if (tokenIsValid) {
+            initialRoute = '/';
+        } else {
+            initialRoute = '/login';
+            this._cookieStorage.deleteValue(CookieStorage.TOKEN_STRING);
+        }
+
         SystemChrome.setEnabledSystemUIOverlays([]);
-        
+
         return new MaterialApp(
             debugShowCheckedModeBanner: true,
             theme: AppThemes.getDarkModeAppTheme,
-            initialRoute: this._initialRoute,
+            initialRoute: initialRoute,
             routes: routes
         );
     }
