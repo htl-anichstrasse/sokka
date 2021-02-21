412abd7d563387b057e5c66fbb372464987efa0f
Nicho <n.rossi2002@gmail.com>
2021-02-21 22:26:08 +0100

    init fetch service and cookie storage

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,5 +1,8 @@
 import 'dart:ui' as UI;
+import 'package:client/services/BearerAuth.dart';
+import 'package:client/services/FetchOrderables.dart';
 import 'package:client/services/UserAuth.dart';
+import 'package:client/util/CookieStorage.dart';
 import 'package:client/util/Routes.dart';
 import 'package:client/views/LoadingSplashScreen.dart';
 import 'package:flutter/material.dart';
@@ -10,10 +13,14 @@ void main() async => runApp(Sokka());
 
 class Sokka extends StatelessWidget {
     final UserAuth _userAuth = new UserAuth();
+    final FetchOrderables _fetchOrderables = new FetchOrderables();
+    final CookieStorage _cookieStorage = new CookieStorage();
 
     @override
     Widget build(BuildContext context) {
         SystemChrome.setEnabledSystemUIOverlays([]);
+        this._cookieStorage.initializeCache();
+        this._fetchOrderables.initializeMenus();
         
         return new FutureBuilder(
             future: this._userAuth.validateSession(),
