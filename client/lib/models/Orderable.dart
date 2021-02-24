653e0b9bfdcbc3dd8a966ce4166ffdeb761d0971
Nicho <n.rossi2002@gmail.com>
2021-02-24 21:59:32 +0100

    added to interface

--- client/lib/models/Orderable.dart
+++ client/lib/models/Orderable.dart
@@ -1,9 +1,14 @@
+import 'package:flutter/material.dart';
+
 abstract class Orderable {
     String get getName;
     set setName(final String name);
 
     double get getPrice;
     set setPrice(final double price);
+
+    NetworkImage get getImage;
+    set setImage(final NetworkImage image);
 
     bool get isHidden;
     set setIsHidden(final bool isHidden);
