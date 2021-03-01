27dff2f7795620910925ae4f2c4958664c7e1fc2
Bemoty <josh@bemoty.dev>
2021-03-02 00:15:45 +0100

    server: acpvalidate parameter consistency

--- server/src/routes/acp/ACPValidateRoute.ts
+++ server/src/routes/acp/ACPValidateRoute.ts
@@ -15,15 +15,15 @@ class ACPValidateRoute extends Route {
         this.router.post('/validate', this.post.bind(this));
     }
 
-    @NeedsProperties({ token: 'string', username: 'string' })
+    @NeedsProperties({ token: 'string', name: 'string' })
     private async post(req: Request, res: Response): Promise<void> {
         try {
-            let user = await ACPUser.get(req.body.username);
+            let user = await ACPUser.get(req.body.name);
             let result = await ACPSession.validate(user, req.body.token);
             if (user && result) {
                 res.send({ success: true, message: 'ACP token for this email is valid' });
             } else {
-                res.send({ success: false, message: `Could not validate ACP token for username '${req.body.username}'` })
+                res.send({ success: false, message: `Could not validate ACP token for username '${req.body.name}'` })
             }
         } catch (err) {
             if (err.message === 'ACP user not found') {
